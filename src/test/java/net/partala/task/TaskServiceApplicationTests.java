877767a3878ef831327d75c1334fc300a33c9a0c
Alexey Partala <partala.dev@gmail.com>
2025-11-23 20:34:06 +0100

    init

---
+++ src/test/java/net/partala/task/TaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.partala.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
